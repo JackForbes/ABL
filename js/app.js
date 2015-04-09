2d574aba6e9ff201f795c8b4a58bb4aba487838a
JackForbes <jackforbes24@gmail.com>
2015-04-09 01:02:55 -0700

    Material Start

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services'
 ])
-.config(function($routeProvider, $mdThemingProvider) {
+.config(function($routeProvider, $mdThemingProvider, $mdIconProvider) {
   $routeProvider.when('/craigslist', {
     templateUrl: 'partials/craigslist.html',
     controller: 'AdCtrl'
@@ -21,6 +21,15 @@ angular.module('myApp', [
     templateUrl: 'partials/main.html',
     controller: 'AppCtrl'
   });
+
+  $mdIconProvider
+      .defaultIconSet("./assets/svg/avatars.svg", 128)
+      .icon("menu"       , "./assets/svg/menu.svg"        , 24)
+      .icon("share"      , "./assets/svg/share.svg"       , 24)
+      .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
+      .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
+      .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
+      .icon("phone"      , "./assets/svg/phone.svg"       , 512);
 
   $mdThemingProvider.theme('default')
     .primaryPalette('indigo', {
